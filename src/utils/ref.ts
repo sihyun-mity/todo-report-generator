1c3914f3d6c55f1f9fef891dd770de202e99163c
Sihyeon Hong <72115242+sihyun-mity@users.noreply.github.com>
2026-05-18 17:14:40 +0900

    fix(utils): mergeRefs에서 잘못된 RefObject 타입 캐스팅 수정

--- src/utils/ref.ts
+++ src/utils/ref.ts
@@ -1,4 +1,4 @@
-import { MutableRefObject, Ref } from 'react';
+import { Ref, RefObject } from 'react';
 
 export const mergeRefs = <T>(...refs: Array<Ref<T> | undefined>): Ref<T> => {
   return (element: T | null) => {
@@ -6,7 +6,7 @@ export const mergeRefs = <T>(...refs: Array<Ref<T> | undefined>): Ref<T> => {
       if (typeof ref === 'function') {
         ref(element);
       } else if (ref && 'current' in ref) {
-        (ref as MutableRefObject<T | null>).current = element;
+        (ref as RefObject<T | null>).current = element;
       }
     });
   };
